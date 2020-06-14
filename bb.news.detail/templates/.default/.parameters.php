ca8751f4e3dca50cab77a402d587200507223310
Cepech325 <66874886+Cepech325@users.noreply.github.com>
2020-06-14 19:31:19 +0300

    Added template

---
+++ bb.news.detail/templates/.default/.parameters.php
@@ -0,0 +1,26 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arTemplateParameters = array(
+	"DISPLAY_DATE" => Array(
+		"NAME" => "DISPLAY_DATE",
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_NAME" => Array(
+		"NAME" => "DISPLAY_NAME",
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_PICTURE" => Array(
+		"NAME" => "DISPLAY_PICTURE",
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	),
+	"DISPLAY_PREVIEW_TEXT" => Array(
+		"NAME" => "DISPLAY_PREVIEW_TEXT",
+		"TYPE" => "CHECKBOX",
+		"DEFAULT" => "Y",
+	)
+);
+?>
---
+++ bb.news.detail/templates/.default/template.php
@@ -0,0 +1,60 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$this->setFrameMode(true);
+?>
+<div class="news-detail">
+	<?if (!is_array($arResult["DETAIL_PICTURE"]) && is_array($arResult["PREVIEW_PICTURE"]))
+		$arResult["DETAIL_PICTURE"] = $arResult["PREVIEW_PICTURE"];
+	if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arResult["DETAIL_PICTURE"])):?>
+		<img
+			class="detail_picture"
+			border="0"
+			src="<?=$arResult["DETAIL_PICTURE"]["SRC"]?>"
+			width="<?=$arResult["DETAIL_PICTURE"]["WIDTH"]?>"
+			height="<?=$arResult["DETAIL_PICTURE"]["HEIGHT"]?>"
+			alt="<?=$arResult["DETAIL_PICTURE"]["ALT"]?>"
+			title="<?=$arResult["DETAIL_PICTURE"]["TITLE"]?>"
+			/>
+	<?endif?>
+	<?if($arParams["DISPLAY_DATE"]!="N" && $arResult["DISPLAY_ACTIVE_FROM"]):?>
+		<span class="news-date-time"><?=$arResult["DISPLAY_ACTIVE_FROM"]?></span>
+	<?endif;?>
+	<?if($arParams["DISPLAY_NAME"]!="N" && $arResult["NAME"]):?>
+		<h3><?=$arResult["NAME"]?></h3>
+	<?endif;?>
+	<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arResult["FIELDS"]["PREVIEW_TEXT"]):?>
+		<p><?=$arResult["FIELDS"]["PREVIEW_TEXT"];unset($arResult["FIELDS"]["PREVIEW_TEXT"]);?></p>
+	<?endif;?>
+	<?if(strlen($arResult["DETAIL_TEXT"])>0):?>
+		<?echo $arResult["DETAIL_TEXT"];?>
+	<?else:?>
+		<?echo $arResult["PREVIEW_TEXT"];?>
+	<?endif?>
+	<div style="clear:both"></div>
+	<br />
+	<?/*foreach($arResult["FIELDS"] as $code=>$value):
+		if ('PREVIEW_PICTURE' == $code || 'DETAIL_PICTURE' == $code)
+		{
+			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?
+			if (!empty($value) && is_array($value))
+			{
+				?><img border="0" src="<?=$value["SRC"]?>" width="<?=$value["WIDTH"]?>" height="<?=$value["HEIGHT"]?>"><?
+			}
+		}
+		else
+		{
+			?><?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?><?
+		}
+		?><br />
+	<?endforeach;*/
+	foreach($arResult["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
+
+		<?=$arProperty["NAME"]?>:&nbsp;
+		<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
+			<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
+		<?else:?>
+			<?=$arProperty["DISPLAY_VALUE"];?>
+		<?endif?>
+		<br />
+	<?endforeach;?>
+</div>
